cbe3b7a390cf6dc83c5450fa54e04de00281d471
Hernando Cortina <hcortina@justcapital.com>
2017-09-17 11:08:40 -0400

    Classifier works

--- homework0.R
+++ homework0.R
@@ -14,3 +14,12 @@ slope <- (x[2]-xp[2])/(x[1]-xp[1])
 intercept <- x[2]-slope*x[1]
 
 abline(intercept, slope, col="red")
+
+for (i in 1:15) {
+w <- c(intercept, slope, -1)
+z <- c(1,runif(1,-1,1),runif(1,-1,1))
+p <- crossprod(z,w)
+points(z[2],z[3],col='blue',lwd=1,pch=ifelse(p>0,8,15))
+}
+
+rm(list=ls())
